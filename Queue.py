0fe971221c3b5571736b66673453fc2ba3e5d1dc
saianudeepgit <140361870+saianudeepgit@users.noreply.github.com>
2023-09-27 00:01:23 +0530

    Add files via upload

--- Queue.py
+++ Queue.py
@@ -1,46 +1,32 @@
-class node:
-    def __init__(self,data):
-        self.data=data
-        self.next=None
-
 class Queue:
-    def __init__(self):
-        self.front=None
-        self.rear=None
 
-    def enqueue(self,data):
-        node_new=node(data)
-        if self.rear is None:
-           self.front=node_new
-           self.rear=node_new
-        else:
-            self.rear.next=node_new
-            self.rear=node_new
+   def __init__(self):
+       self.items = []
 
-    def dequeue(self):
-        if self.front is None:
-            return None
-        dequeued_data =self.front.data
-        self.front=self.front.next
-        if self.front is None:
-            self.rear is None
-            return dequeued_data
+   def __repr__(self):
+       return f'Queue object: data={self.items}'
 
-    def peek(self):
-        if self.front is None:
-            return None
-        return self.front is None
+   def is_empty(self):
+       return not self.items
 
-    def print_queue(self):
-        current = self.front
-        while current:
-            print(current.data)
-            current=current.next
-        print("None")
+   def enqueue(self, item):
+       self.items.append(item)
 
+   def dequeue(self):
+       return self.items.pop(0)
 
-queue=Queue()
-queue.enqueue(101)
-queue.enqueue(102)
-queue.enqueue(103)
-queue.print_queue()
+   def size(self):
+       return len(self.items)
+
+   def peek(self):
+       return self.items[0]
+if __name__ == '__main__':
+   q = Queue()
+   print(q.is_empty())
+   q.enqueue('First')
+   q.enqueue('Second')
+   print(q)
+   print(q.dequeue())
+   print(q)
+   print(q.size())
+   print(q.peek())
