f8b835965b06577997fbffcff734530779b42dbb
saianudeepgit <140361870+saianudeepgit@users.noreply.github.com>
2023-09-26 23:43:38 +0530

    Add files via upload

---
+++ RemoveEmptyTupleswithLoops.py
@@ -0,0 +1,9 @@
+
+def Remove(tuples):
+    while () in tuples:
+        tuples.remove(())
+    return tuples
+
+tuples = [(), ('ram','15','8'), (), ('laxman', 'sita'),
+        ('krishna', 'akbar', '45'), ('',''),()]
+print (Remove(tuples))
---
+++ numpyTest.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+a = np.array([1,2,3], dtype='int32')
+print(a)
+
+b = np.array([[9.0,8.0,7.0],[6.0,5.0,4.0]])
+print(b)
+
+pria.ndim
+b.shape
+a.dtype
+a.itemsize
+a.nbytes
+a.size
